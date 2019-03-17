9c2af2f189cdf0d0a31ec63b29183ad14f8b5e43
Ming <stevenzhongming@outlook.com>
2019-03-17 16:09:04 -0400

    ?

--- lib/feature.R
+++ lib/feature.R
@@ -2,9 +2,8 @@
 ### Construct features and responses for training images###
 #############################################################
 
-### Authors: Chengliang Tang/Tian Zheng
+### Authors: Yixin Zhang
 ### Project 3
-
 feature <- function(LR_dir, HR_dir, n_points=1000){
   
   ### Construct process features for training images (LR/HR pairs)
@@ -25,17 +24,35 @@ feature <- function(LR_dir, HR_dir, n_points=1000){
   for(i in 1:n_files){
     imgLR <- readImage(paste0(LR_dir,  "img_", sprintf("%04d", i), ".jpg"))
     imgHR <- readImage(paste0(HR_dir,  "img_", sprintf("%04d", i), ".jpg"))
+    dimLR <- dim(imgLR)
+    
     ### step 1. sample n_points from imgLR
+    pixels <- sample(length(imgLR[ , ,1]), n_points)
+    r <- (pixels-1)%%dimLR[1]+1
+    c <- ceiling(pixels/dimLR[1])
     
     ### step 2. for each sampled point in imgLR,
-    
-        ### step 2.1. save (the neighbor 8 pixels - central pixel) in featMat
-        ###           tips: padding zeros for boundary points
-    
-        ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
-    
-    ### step 3. repeat above for three channels
+    ### step 2.1. save (the neighbor 8 pixels - central pixel) in featMat
+    ###           tips: padding zeros for boundary points
+    for (j in 1:3) {
+      pad <- cbind(0, imgLR[,,j], 0)
+      pad <- rbind(0, pad, 0)
+      featMat[(i-1)*n_points+1:n_points,1,j] <- pad[cbind(r,c)] 
+      featMat[(i-1)*n_points+1:n_points,2,j] <- pad[cbind(r,c+1)] 
+      featMat[(i-1)*n_points+1:n_points,3,j] <- pad[cbind(r,c+2)] 
+      featMat[(i-1)*n_points+1:n_points,4,j] <- pad[cbind(r+1,c)] 
+      featMat[(i-1)*n_points+1:n_points,5,j] <- pad[cbind(r+1,c+2)] 
+      featMat[(i-1)*n_points+1:n_points,6,j] <- pad[cbind(r+2,c)] 
+      featMat[(i-1)*n_points+1:n_points,7,j] <- pad[cbind(r+2,c+1)] 
+      featMat[(i-1)*n_points+1:n_points,8,j] <- pad[cbind(r+2,c+2)]
       
+      ### step 2.2. save the corresponding 4 sub-pixels of imgHR in labMat
+      labMat[(i-1)*n_points+1:n_points,1,j] <- imgHR[cbind(2*r-1,2*c-1,j)]
+      labMat[(i-1)*n_points+1:n_points,2,j] <- imgHR[cbind(2*r,2*c-1,j)]
+      labMat[(i-1)*n_points+1:n_points,3,j] <- imgHR[cbind(2*r-1,2*c,j)]
+      labMat[(i-1)*n_points+1:n_points,4,j] <- imgHR[cbind(2*r,2*c,j)]
+    }
   }
   return(list(feature = featMat, label = labMat))
 }
+
